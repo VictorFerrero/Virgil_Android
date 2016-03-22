fa41282e3b420b963793ad1e41741b55c1a05285
Ty Talafous <ttalafous@gmail.com>
2016-03-22 06:33:08 -0500

    Changed adapter for alternating colors

--- Virgil/app/src/main/java/wisc/virgil/virgil/MuseumListAdapter.java
+++ Virgil/app/src/main/java/wisc/virgil/virgil/MuseumListAdapter.java
@@ -31,6 +31,12 @@ public class MuseumListAdapter extends ArrayAdapter<Museum> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        if(position % 2 == 0) {
+            this.layoutResourceId = R.layout.museum_list_item;
+        } else {
+            this.layoutResourceId = R.layout.museum_list_item_dark;
+        }
+
         if(convertView == null) {
             LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
             convertView = inflater.inflate(layoutResourceId, parent, false);
@@ -38,6 +44,42 @@ public class MuseumListAdapter extends ArrayAdapter<Museum> {
 
         Museum museum = data.get(position);
 
+        //Alternates the color of each list item
+        //TODO: Slow as it requires redrawing every other list item
+        /*
+        if(position % 2 == 0) {
+            convertView.setBackgroundColor(
+                    convertView.getResources().getColor(R.color.colorPrimaryDark));
+            convertView.invalidate();
+        }
+        */
+
+        //Alternative that changes resourceId every time
+        if(position % 2 == 0) {
+            TextView museumName = (TextView) convertView.findViewById(R.id.tv_museumName);
+            museumName.setText(museum.getName());
+            TextView museumAddress = (TextView) convertView.findViewById(R.id.tv_museumAddress);
+            museumAddress.setText(museum.getAddress());
+            //TODO: Add hours field
+            //TextView museumHours = (TextView) convertView.findViewById(R.id.tv_museumListHours);
+            //museumHours.setText(museum.getAddress());
+            //TODO: Add museum list images
+            //ImageView museumImage = (ImageView) convertView.findViewById(R.id.iv_museumListImage);
+            //museumImage.setImageDrawable();
+        } else {
+            TextView museumName = (TextView) convertView.findViewById(R.id.tv_museumNameDark);
+            museumName.setText(museum.getName());
+            TextView museumAddress = (TextView) convertView.findViewById(R.id.tv_museumAddressDark);
+            museumAddress.setText(museum.getAddress());
+            //TODO: Add hours field
+            //TextView museumHours = (TextView) convertView.findViewById(R.id.tv_museumListHoursDark);
+            //museumHours.setText(museum.getAddress());
+            //TODO: Add museum list images
+            //ImageView museumImage = (ImageView) convertView.findViewById(R.id.iv_museumListImageDark);
+            //museumImage.setImageDrawable();
+        }
+
+        /*
         TextView museumName = (TextView) convertView.findViewById(R.id.tv_museumName);
         museumName.setText(museum.getName());
         TextView museumAddress = (TextView) convertView.findViewById(R.id.tv_museumAddress);
@@ -48,6 +90,7 @@ public class MuseumListAdapter extends ArrayAdapter<Museum> {
         //TODO: Add museum list images
         //ImageView museumImage = (ImageView) convertView.findViewById(R.id.iv_museumListImage);
         //museumImage.setImageDrawable();
+        */
 
         return convertView;
     }
